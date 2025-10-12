6f7c91d9a7a8529db5e04658704e62ccbaf504ad
AkimBerreqia <berreqia.akim@gmail.com>
2025-10-12 20:47:06 +0200

    Update jump + climb
    - The player can most of the time rejump smoothly and continuously if he wants to.
    
    - The player can now climb while pressing "j" (added climb option in the Input Manager), BUT :
    -- The player falls gently while climbing
    -- The player gets violently ejected to the sky if he reaches the end of the wall, while climbing
    -- The player cannot wall jump in the opposite direction from its starting direction on the wall, yet
    -- The player can climb at a small distance from the wall, looks like he's flying :'(

--- Last Apprentice/Assets/Scripts/Movement.cs
+++ Last Apprentice/Assets/Scripts/Movement.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Drawing;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
@@ -13,8 +9,13 @@ public class Movement : MonoBehaviour
     [SerializeField] private SpriteRenderer spriteRenderer;
 
     private Vector2 movement;
-    private float input;
+    private float runInput;
+    private float climbInput;
+    private float wallGrabDirection;
     private float playerHalfHeight;
+
+    private RaycastHit2D isDiagonallyGrounded;
+    private RaycastHit2D isDownGrounded;
 
     private void Start()
     {
@@ -24,35 +25,85 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Player moves with "A"/"D" or "left"/"right"
-        input = Input.GetAxis("Horizontal");
-        movement.x = input * speed * Time.deltaTime;
-
-        transform.Translate(movement);
-
-        if (Input.GetButtonDown("Jump") && GetIsGrounded())
+        // Check the wall grab direction, to know on which side the player is able to climb
+        if (runInput != 0)
         {
-            Jump();
+            wallGrabDirection = runInput;
         }
 
-        if (rb.linearVelocity.y < 0)
+        if (Input.GetButtonDown("Jump"))
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y + (fallMultiplier - 1) * Physics2D.gravity.y * Time.deltaTime);
-        } else if (Input.GetButtonUp("Jump") && !GetIsGrounded() && rb.linearVelocity.y > 0)
+            if (GetIsGrounded())
+            {
+                Jump();
+            } else if (GetCanClimb())
+            {
+                WallJump();
+            }
+        }
+
+        if (Input.GetButton("Climb") && GetCanClimb())
+        {
+            Climb();
+        } else
+        {
+            Run();
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * .1f);
+        } else if (rb.linearVelocity.y <= 0)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y + (fallMultiplier - 1) * Physics2D.gravity.y * Time.deltaTime);
         }
     }
 
-    private bool GetIsGrounded()
+    private void Run()
     {
-        // "Vector2(-input, 0)" is used to help player jumping, even if he has passed a bit of the platform (he has more time to jump)
-        // "playerHalfHeight * 1.5f" scales the range of the Raycast if the player scale changes
-        return Physics2D.Raycast(transform.position, Vector2.down + new Vector2(-input, 0), playerHalfHeight * 2f, LayerMask.GetMask("Ground"));
+        // Player moves with "A"/"D" or "left"/"right"
+        runInput = Input.GetAxis("Horizontal");
+        movement.x = runInput * speed * Time.deltaTime;
+
+        transform.Translate(movement);
+    }
+    private void Climb()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0f);
+
+        // Player moves with "W"/"S" or "up"/"down"
+        climbInput = Input.GetAxis("Vertical");
+        movement = new Vector2(0, climbInput * speed * Time.deltaTime);
+
+        transform.Translate(movement);
     }
 
     private void Jump()
     {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0f);
         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void WallJump()
+    {
+        movement.x = wallGrabDirection * speed * Time.deltaTime;
+        Jump();
+    }
+
+    private bool GetIsGrounded()
+    {
+        // Checks if there is a platform under the player only
+        isDownGrounded = Physics2D.BoxCast(transform.position, spriteRenderer.bounds.size, 0, Vector2.down, playerHalfHeight * .75f, LayerMask.GetMask("Ground"));
+        
+        // "Vector2(-runInput, 0)" is used to help player jumping, even if he has passed a bit of the platform (he has more time to jump)
+        // "playerHalfHeight * 1.5f" scales the range of the Raycast if the player scale changes
+        isDiagonallyGrounded = Physics2D.BoxCast(transform.position, spriteRenderer.bounds.size, 0, Vector2.down + new Vector2(-runInput, 0), playerHalfHeight * .75f, LayerMask.GetMask("Ground"));
+
+        return (isDownGrounded || isDiagonallyGrounded);
+    }
+
+    private bool GetCanClimb()
+    {
+        return Physics2D.BoxCast(transform.position, spriteRenderer.bounds.size, 0, new Vector2(wallGrabDirection, 0), playerHalfHeight * .75f, LayerMask.GetMask("Ground"));
     }
 }
